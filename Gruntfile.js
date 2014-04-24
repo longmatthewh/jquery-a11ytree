f281f91bf8f9aa50020b1f1d6fd1463299afa52f
longmatthewh <longmatthewh@gmail.com>
2014-04-23 21:57:54 -0400

    adding jasmine to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,10 +14,17 @@ module.exports = function(grunt) {
         },
         jshint: {
             all: ['src/js/<%= pkg.name %>.js']
+        },
+        jasmine: {
+            src: ['src/js/lib/jquery-1.11.0.js','src/js/jquery.a11yTree.js'],
+            options: {
+                specs: 'test/js/specs/a11yTreeSpec.js'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['uglify', 'jshint']);
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.registerTask('default', ['uglify', 'jshint', 'jasmine']);
 };
